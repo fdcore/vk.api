b45dfdd31813223433d538471cd9fe25854380af
Дмитрий Няшкин <admin@fdcore.com>
2015-10-21 11:09:35 +0300

    delete

--- example/wall_delete.php
+++ example/wall_delete.php
@@ -1,20 +1,8 @@
 <?php
 // http://vk.com/dev/wall.delete
 
-ini_set('display_errors', true);
-include('../vk.api.php');
-include('print_var.php');
-include('lime.php');
-include('config.php');
-
+require '_connect_vk.php';
 
 $v = new Vk($config);
 
-// test case
-$t = new lime_test(1);
-
 $response = $v->api('wall.delete', array('post_id' => 0));
-
-print_var($response);
-
-$t->isa_ok($response[0], 'integer', 'return 1 for success');
